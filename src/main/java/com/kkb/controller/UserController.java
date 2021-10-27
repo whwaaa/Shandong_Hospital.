3bb8bb7ab50ea2343a3d037bf186af75d1752ca4
hanweiwu <8838261+hanweiwu@user.noreply.gitee.com>
2021-10-28 02:01:09 +0800

    Frame structures

--- src/main/java/com/kkb/controller/UserController.java
+++ src/main/java/com/kkb/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.kkb.controller;
 
 import com.kkb.service.UserService;
+import com.kkb.vo.AjaxResultVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Shandong_Hospital
@@ -13,9 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @date 2021/10/28
  */
 @Controller
+@ResponseBody
 @RequestMapping("user")
 public class UserController {
 
     @Autowired
     private UserService userService;
+
 }
