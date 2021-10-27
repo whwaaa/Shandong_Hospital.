4309d046ff79bbf2b89d1f85a92b61e497e52b5b
hanweiwu <8838261+hanweiwu@user.noreply.gitee.com>
2021-10-28 01:58:50 +0800

    Frame structures

--- src/main/java/com/kkb/controller/UserController.java
+++ src/main/java/com/kkb/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.kkb.controller;
 
+import com.kkb.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,4 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("user")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
 }
